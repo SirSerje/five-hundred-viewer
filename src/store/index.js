19aa9e4eb6f98999b997f38a5ae2df6fe582a651
SirSerje <sir.serje@gmail.com>
2018-02-27 19:53:19 +0200

    add lodash.uniq and fix init redux devtool

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,13 @@
-import { createStore, applyMiddleware } from 'redux';
+import { compose, createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/index';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const hasWindow = typeof window === 'object'
+const composeEnhancers = (hasWindow && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 
-export default function configureStore(initialState) {
-    return createStore(rootReducer ,/* preloadedState, */ composeEnhancers(
+export default function configureStore(preloadedState ) {
+    return createStore(rootReducer ,preloadedState,  composeEnhancers(
         applyMiddleware(thunk)
     ))
 }
